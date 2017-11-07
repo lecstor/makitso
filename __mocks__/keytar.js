b2bb96bfb9a586bdaa046f1d5bb589a1616feac2
Jason Galea <jason@lecstor.com>
2017-11-08 00:17:36 +1000

    add keychain store and tests

---
+++ __mocks__/keytar.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const keytar = jest.genMockFromModule("keytar");
+
+let mockKeychain = Object.create(null);
+
+function secretKey(service, account) {
+  return `${service}-${account}`;
+}
+
+async function setPassword(service, account, password) {
+  console.log("SETPASSWORD");
+  const key = secretKey(service, account);
+  mockKeychain[key] = password;
+}
+
+async function getPassword(service, account) {
+  const key = secretKey(service, account);
+  if (!mockKeychain[key]) {
+    return null;
+  }
+  return mockKeychain[key];
+}
+
+async function deletePassword(service, account) {
+  const key = secretKey(service, account);
+  if (!mockKeychain[key]) {
+    return false;
+  }
+  delete mockKeychain[key];
+  return true;
+}
+
+keytar.setPassword = setPassword;
+keytar.getPassword = getPassword;
+keytar.deletePassword = deletePassword;
+
+module.exports = keytar;

a0af34afa3a22f30e0fa6c718d523c4d449d9313
Jason Galea <jason@lecstor.com>
2017-12-20 21:44:50 +1000

    committing all the all the protos and moving to makitso-prompt

--- pure-readline.js
+++ pure-readline.js
@@ -3,18 +3,13 @@ http://slides.com/glenarrowsmith/writing-node-js-command-line-tools#/
 http://thisdavej.com/making-interactive-node-js-console-apps-that-listen-for-keypress-events/
 */
 
+// const readline = require("./lib/command/readline");
 const readline = require("readline");
 const chalk = require("chalk");
+const termSize = require("term-size");
+const stringWidth = require("string-width");
+const _forEach = require("lodash/forEach");
 const debug = require("./lib/debug");
-
-const nonPrintable = {
-  tab: true,
-  up: true,
-  down: true,
-  left: true,
-  right: true,
-  backspace: true
-};
 
 function Makitso(arg0) {
   const {
@@ -24,64 +19,22 @@ function Makitso(arg0) {
     readline: rl = readline.createInterface({ input, output })
   } = arg0;
 
+  rl.setPrompt(prompt);
+
   const makitso = {
     rl,
-    command: "",
-    cursorX: 0,
-    prompt,
-    history: [""],
-    historyIdx: -1,
 
-    cursorMove(n) {
-      if (this.cursorX + n >= 0 && this.cursorX + n <= this.command.length) {
-        this.cursorX = this.cursorX + n;
-        return true;
-      }
-      return false;
+    updateTerminalSize() {
+      this.size = termSize();
     },
 
-    commandInsert(idx, str) {
-      const prefix = this.command.slice(0, idx);
-      const suffix = this.command.slice(idx);
-      debug({ prefix, suffix, x: idx });
-      this.command = `${prefix}${str}${suffix}`;
-      this.cursorMove(str.length);
-    },
-
-    commandRemove(idx) {
-      if (this.cursorMove(-1)) {
-        const prefix = this.command.slice(0, idx);
-        const suffix = this.command.slice(idx + 1);
-        debug({ prefix, suffix, x: idx });
-        this.command = `${prefix}${suffix}`;
-      }
-    },
-
-    commandSet(command) {
-      this.command = command;
-      this.cursorX = command.length;
-    },
-
-    commandClear() {
-      if (this.history[0]) {
-        this.history.unshift("");
-      }
-      this.historyIdx = 0;
-      this.command = "";
-      this.cursorX = 0;
-    },
+    defaultPrompt: prompt,
 
     setPrompt(newPrompt) {
-      this.prompt = newPrompt || prompt;
+      this.rl.setPrompt(newPrompt || this.defaultPrompt);
     },
 
     onKeyPress(input, key) {
-      debug({
-        key: key.name,
-        history: this.history,
-        historyIdx: this.historyIdx
-      });
-      this.rl.line = "";
       if (key.name === "enter" || key.name === "return") {
         return;
       }
@@ -89,52 +42,187 @@ function Makitso(arg0) {
         this.close();
         return;
       }
-      if (nonPrintable[key.name]) {
-        if (key.name === "left") {
-          this.cursorMove(-1);
-        }
-        if (key.name === "right") {
-          this.cursorMove(+1);
-        }
-        if (key.name === "up") {
-          if (this.historyIdx < this.history.length - 1) {
-            this.historyIdx++;
-            this.commandSet(this.history[this.historyIdx]);
-          }
-        }
-        if (key.name === "down") {
-          if (this.historyIdx > 0) {
-            this.historyIdx--;
-            this.commandSet(this.history[this.historyIdx]);
-          }
-        }
-        if (key.name === "backspace") {
-          this.commandRemove(this.cursorX - 1);
-        }
-        this.processKeyPress(input, key);
-        this.render();
-        return;
-      }
-      if (!key.ctrl) {
-        this.commandInsert(this.cursorX, key.sequence);
-        this.history[0] = this.command;
-      }
-      this.processKeyPress(input, key);
       this.render();
     },
 
-    onLine(line) {
-      debug({ line });
-      const command = this.command.trim();
+    onSubmit(line) {
+      this.clearScreenDown();
+      console.log(`Submitted: ${line}`);
+      this.render();
+    },
 
-      if (/^help\b/.test(command)) {
-        readline.clearScreenDown(output);
-        this.displayHelp(command.replace(/^help\s*/, ""));
-        this.commandClear();
-        this.render();
-        return;
+    clearScreenDown() {
+      readline.clearScreenDown(output);
+    },
+
+    deleteLeft() {
+      this.rl._deleteLeft();
+    },
+
+    insertString(str) {
+      this.rl._insertString(str);
+    },
+
+    close() {
+      this.rl.close();
+    },
+
+    onClose() {
+      process.exit(0);
+    },
+
+    onPause() {},
+
+    onResume() {},
+
+    renderPrompt({ prompt, command }) {
+      return {
+        before: [
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non neque ut massa sollicitudin condimentum. Cras facilisis enim non semper.\n",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non neque ut massa sollicitudin condimentum. Cras facilisis enim non semper.\n"
+        ],
+        prompt: chalk`{blue ${prompt}${command}}`,
+        after: [
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non neque ut massa sollicitudin condimentum. Cras facilisis enim non semper.\n",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non neque ut massa sollicitudin condimentum. Cras facilisis enim non semper."
+        ]
+      };
+    },
+
+    renderState: {
+      before: [],
+      prompt: "placeholder",
+      after: []
+    },
+
+    linesHeight(lines) {
+      const { columns } = this.size;
+      return lines.reduce((sum, line) => {
+        const width = stringWidth(line);
+        sum += Math.ceil(width / columns);
+        return sum;
+      }, 0);
+    },
+
+    getRenderState() {
+      const state = this.renderState;
+      // get fresh in case terminal width has changed
+      state.beforeHeight = this.linesHeight(state.before);
+      if (makitso.terminalSizeUpdated) {
+        state.promptHeight = this.linesHeight([state.prompt]);
+      }
+      makitso.terminalSizeUpdated = false;
+      return state;
+    },
+
+    getPromptUpHeights(cursorPos) {
+      const state = this.getRenderState();
+
+      // height from prompt to top of lines
+      // cursorPos.rows > 0 tells us we're not on the first line of the prompt
+      // due to terminal wrapping.
+      let topHeight = state.beforeHeight + cursorPos.rows;
+
+      const promptHeight = this.linesHeight([prompt]);
+      const promptWidth = stringWidth(prompt);
+
+      const promptHeightChanged = state.promptHeight !== promptHeight;
+      const promptWidthChanged = stringWidth(state.prompt) !== promptWidth;
+
+      if (promptHeightChanged && !promptWidthChanged) {
+        // it was the terminal in the computer with the width
+        topHeight -= state.promptHeight - promptHeight;
       }
 
+      return { topHeight, promptHeight };
+    },
+
+    writeLines({ before, prompt, after }) {
+      _forEach(before, line => rl.output.write(line));
+      rl.output.write(prompt.trim() + "\n");
+      _forEach(after, line => rl.output.write(line));
+    },
+
+    render() {
+      this.renderPromise = this.renderAsync().catch(error => {
+        console.error(error);
+        process.exit(1);
+      });
+    },
+
+    renderAsync: async function() {
+      const { before, prompt, after } = await this.renderPrompt({
+        prompt: this.rl._prompt,
+        command: this.rl.line
+      });
+
+      let cursorPos = this.rl._getCursorPos();
+
+      const { topHeight, promptHeight } = this.getPromptUpHeights(cursorPos);
+
+      readline.cursorTo(output, 0);
+      readline.moveCursor(output, 0, -topHeight);
+      this.clearScreenDown();
+
+      this.writeLines({ before, prompt, after });
+
+      cursorPos = this.rl._getCursorPos();
+
+      const bottomHeight =
+        this.linesHeight(after) + promptHeight - 1 - cursorPos.rows;
+
+      readline.moveCursor(output, 0, -bottomHeight);
+      readline.cursorTo(output, cursorPos.cols);
+
+      this.renderState = { before, prompt, after, promptHeight };
+    }
+  };
+
+  readline.emitKeypressEvents(input);
+  input.on("keypress", (input, key) => makitso.onKeyPress(input, key));
+
+  rl.on("line", line => makitso.onSubmit(line));
+  rl.on("close", () => makitso.onClose());
+  rl.on("pause", () => makitso.onPause());
+  rl.on("resume", () => makitso.onResume());
+
+  process.stdout.on("resize", function() {
+    makitso.updateTerminalSize();
+    makitso.terminalSizeUpdated = true;
+    makitso.render();
+  });
+  makitso.updateTerminalSize();
+
+  return makitso;
+}
+
+const makitso = Makitso({ prompt: "makitso> " });
+
+const overrides = {
+  onKeyPress(input, key) {
+    if (key.name === "enter" || key.name === "return") {
+      return;
+    }
+    if (key.ctrl && key.name === "c") {
+      this.close();
+      return;
+    }
+    if (key.name === "tab") {
+      this.deleteLeft(); // remove the tab
+      this.insertString("HHHH");
+    }
+    this.render();
+  },
+
+  onSubmit(line) {
+    debug({ line });
+    const command = line.trim();
+
+    this.clearScreenDown();
+
+    if (/^help\b/.test(command)) {
+      this.displayHelp(command.replace(/^help\s*/, ""));
+    } else {
       switch (command) {
         case "q":
         case "quit":
@@ -145,86 +233,26 @@ function Makitso(arg0) {
           this.processCommand(command);
           break;
       }
-    },
-
-    displayHelp() {
-      console.log("Help...");
-    },
-
-    setCommandline(newLine) {
-      this.command = newLine;
-    },
-
-    appendCommandline(suffix) {
-      this.setCommandline(`${this.command}${suffix}`);
-    },
-
-    processKeyPress(input, key) {
-      if (key.name === "tab") {
-        this.appendCommandline("bbbb");
-      }
-    },
-
-    processCommand(command) {
-      // console.log(`Say what? I might have heard '${line.trim()}'`);
-      console.log(`Say what? I might have heard '${command}'`);
-      this.commandClear();
-      this.render();
-    },
-
-    close() {
-      this.rl.close();
-    },
-
-    onClose() {
-      console.log("\nHave a great day!");
-      process.exit(0);
-    },
-
-    onPause() {
-      debug("pause");
-    },
-
-    onResume() {
-      debug("resume");
-    },
-
-    render() {
-      readline.cursorTo(output, 0);
-      readline.clearLine(output, 0);
-
-      const cmdline = chalk`{blue ${this.prompt}${this.command}}
-commands: help sos blah
-something something`;
-      rl.output.write(cmdline.trim());
-      debug({ cmdline, length: cmdline.length });
-      readline.cursorTo(output, this.cursorX + this.prompt.length);
-      readline.moveCursor(output, 0, -2);
     }
-  };
+    this.render();
+  },
 
-  readline.emitKeypressEvents(input);
-  input.on("keypress", (input, key) => makitso.onKeyPress(input, key));
-
-  rl.on("line", line => makitso.onLine(line));
-  rl.on("close", () => makitso.onClose());
-  rl.on("pause", () => makitso.onPause());
-  rl.on("resume", () => makitso.onResume());
-
-  rl.line = prompt;
-
-  makitso.render();
-
-  return makitso;
-}
-
-const makitso = Makitso({ prompt: "makitso> " });
-
-const overrides = {
   displayHelp(command) {
-    this.setPrompt("help> ");
     console.log(`Help for ${command || "everything"}\nhth\n`);
+    this.setPrompt("help> ");
+    this.render();
+  },
+
+  processCommand(command) {
+    console.log(`I don't know how to ${command}.`);
+  },
+
+  onClose() {
+    console.log("\nHave a great day!");
+    process.exit(0);
   }
 };
 
-Object.assign(makitso, overrides);
+// Object.assign(makitso, overrides);
+
+makitso.render();

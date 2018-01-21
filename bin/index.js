21ba083106e5c1b01863c97022092d9010664800
Jason Galea <jason@lecstor.com>
2018-01-22 08:24:51 +1000

    update schema and commands structure

--- bin/index.js
+++ bin/index.js
@@ -12,127 +12,133 @@ const Makitso = require("../");
 const tunnelPlugin = require("makitso-tunnelblick2fa-plugin").plugin();
 const developPlugin = require("makitso-develop-plugin").plugin();
 
+const schema = {
+  my: {
+    name: {
+      store: "file",
+      ask: {
+        header: `We'll only ask once..`,
+        prompt: `{variant} name> `,
+        footer: `Run this command again when you're done.`,
+        storedValueIs: "response"
+      }
+    }
+  },
+
+  prompt: {
+    withDefault: {
+      // if there is a stored value, does not prompt
+      store: "file",
+      ask: {
+        header: `A prompt with set default..`,
+        prompt: `prompt> `,
+        default: "help",
+        storedValueIs: "response"
+      }
+    },
+    valueDefault: {
+      // always prompts
+      store: "file",
+      ask: {
+        header: `A prompt with value as default..`,
+        prompt: `prompt> `,
+        storedValueIs: "default"
+      }
+    },
+    valueDefaultWithDefault: {
+      // always prompts
+      store: "file",
+      ask: {
+        header: `A prompt with values default or set default..`,
+        prompt: `prompt> `,
+        default: "help",
+        storedValueIs: "default"
+      }
+    },
+    always: {
+      // always prompts
+      store: "file",
+      ask: {
+        header: `A prompt with set default..`,
+        prompt: `prompt> `,
+        default: "help"
+      }
+    }
+  }
+};
+
+const commands = {
+  printName: {
+    description: "Get and print full name",
+    action: async ({ context }) => {
+      const firstName = await context.get("my.name.first");
+      const lastName = await context.get("my.name.last");
+      console.log(`${firstName} ${lastName}`);
+    }
+  },
+
+  promptWDefault: {
+    description: "A prompt with set default..",
+    action: async ({ context }) => {
+      const command = await context.get("prompt.withDefault");
+      console.log(`${command}`);
+    }
+  },
+  promptVDefault: {
+    description: "A prompt with value as default..",
+    action: async ({ context }) => {
+      const command = await context.get("prompt.valueDefault");
+      console.log(`${command}`);
+    }
+  },
+  promptVDefaultWDefault: {
+    description: "A prompt with value as default or set default..",
+    action: async ({ context }) => {
+      const command = await context.get("prompt.valueDefaultWithDefault");
+      console.log(`${command}`);
+    }
+  },
+  parseArgs: {
+    description: "Test some args and options parsing",
+    arguments: [
+      "firstname {string} your first name",
+      "lastname {string} your last name",
+      "age {number} your real age",
+      "rank {string} your rank",
+      "[likes[]] {string} things you like"
+    ],
+    options: ["-d --doe a dear", "-f --far a long way"],
+    action: async ({ context, input }) => {
+      console.log(input);
+    },
+    choices: async ({ context, input }) => {
+      if (!input.args.fname) {
+        return [];
+      }
+      if (!input.args.sname) {
+        return [];
+      }
+      if (!input.args.age) {
+        return [];
+      }
+      const ranks = ["Colonel", "Lieutenant", "Major"];
+      if (!_includes(ranks, input.args.rank)) {
+        return ranks;
+      }
+      if (!input.args.likes || !input.args.likes.length) {
+        return ["What you like"];
+      }
+    }
+  }
+};
+
 const localPlugin = {
-  schema: {
-    my: {
-      name: {
-        store: "file",
-        prompt: {
-          header: `We'll only ask once..`,
-          prompt: `{variant} name> `,
-          footer: `Run this command again when you're done.`
-        }
-      }
-    },
-
-    prompt: {
-      withDefault: {
-        // if there is a stored value, does not prompt
-        store: "file",
-        ask: {
-          header: `A prompt with set default..`,
-          prompt: `prompt> `,
-          default: "help"
-        }
-      },
-      valueDefault: {
-        // always prompts
-        store: "file",
-        ask: {
-          header: `A prompt with value as default..`,
-          prompt: `prompt> `,
-          valueAsDefault: true
-        }
-      },
-      valueDefaultWithDefault: {
-        // always prompts
-        store: "file",
-        ask: {
-          header: `A prompt with values default or set default..`,
-          prompt: `prompt> `,
-          default: "help",
-          valueAsDefault: true
-        }
-      },
-      always: {
-        // always prompts
-        store: "file",
-        ask: {
-          header: `A prompt with set default..`,
-          prompt: `prompt> `,
-          default: "help",
-          when: "always"
-        }
-      }
-    }
-  },
+  schema,
   commands: {
-    printName: {
-      description: "Get and print full name",
-      action: async ({ context }) => {
-        const firstName = await context.get("my.name.first");
-        const lastName = await context.get("my.name.last");
-        console.log(`${firstName} ${lastName}`);
-      }
-    },
-
-    promptWDefault: {
-      description: "A prompt with set default..",
-      action: async ({ context }) => {
-        const command = await context.get("prompt.withDefault");
-        console.log(`${command}`);
-      }
-    },
-    promptVDefault: {
-      description: "A prompt with value as default..",
-      action: async ({ context }) => {
-        const command = await context.get("prompt.valueDefault");
-        console.log(`${command}`);
-      }
-    },
-    promptVDefaultWDefault: {
-      description: "A prompt with value as default or set default..",
-      action: async ({ context }) => {
-        const command = await context.get("prompt.valueDefaultWithDefault");
-        console.log(`${command}`);
-      }
-    },
-    parseArgs: {
-      description: "Test some args and options parsing",
-      arguments: [
-        "firstname {string} your first name",
-        "lastname {string} your last name",
-        "age {number} your real age",
-        "rank {string} your rank",
-        "[likes[]] {string} things you like"
-      ],
-      options: ["-d --doe a dear", "-f --far a long way"],
-      action: async ({ context, input }) => {
-        console.log(input);
-      },
-      choices: async ({ context, input }) => {
-        if (!input.args.fname) {
-          return [];
-        }
-        if (!input.args.sname) {
-          return [];
-        }
-        if (!input.args.age) {
-          return [];
-        }
-        const ranks = ["Colonel", "Lieutenant", "Major"];
-        if (!_includes(ranks, input.args.rank)) {
-          return ranks;
-        }
-        if (!input.args.likes || !input.args.likes.length) {
-          return ["What you like"];
-        }
-      }
+    demo: {
+      ...commands,
+      description: "Some demos to try"
     }
-  },
-  config: {
-    command: "demo",
-    description: "Some demos to try"
   }
 };
 
